d2d2b20936d4976a8397d94f6c86cae225170df8
GBJBUZZ <127760187+GBJBUZZ@users.noreply.github.com>
2024-11-09 23:11:04 +0530

    Update PollSurvey.jsx

--- src/PollSurvey/PollSurvey.jsx
+++ src/PollSurvey/PollSurvey.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import PollAndSurvey from '../components/pollandsurvey.js'; // or .jsx as appropriate
+import PollAndSurvey from '../components/PollAndSurvey';
 import Navbar from "../Navbar";
 import Footer from "../Footer";
 
